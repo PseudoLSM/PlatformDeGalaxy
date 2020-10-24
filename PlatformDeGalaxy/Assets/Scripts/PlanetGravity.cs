403b6158e02ccb0b2c4735af88ca219c7332a72b
Anders <72574286+PseudoLSM@users.noreply.github.com>
2020-10-24 16:04:13 -0700

    Added a partial Gravity script. It doesnt work

---
+++ PlatformDeGalaxy/Assets/Scripts/PlanetGravity.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+namespace Scripts
+{
+    public class PlanetGravity : MonoBehaviour
+    {
+        public float PullRadius;
+        public float GravitationalPull;
+        public float MinRadius;
+        public float DistanceMultiplier;
+
+        public LayerMask LayersToPull;
+
+        void FixedUpdate()
+        {
+            Collider[] colliders = Physics.OverlapSphere(transform.position, PullRadius, LayersToPull);
+
+            foreach (var collider in colliders)
+            {
+                Rigidbody2D rb;// = collider.GetComponent<Rigidbody2D>();
+
+                if (rb = null) continue;
+
+                Vector2 direction = transform.position - collider.transform.position;
+
+                if (direction.magnitude < MinRadius) continue;
+
+                float distance = direction.sqrMagnitude * DistanceMultiplier + 1;
+
+                rb.AddForce(direction.normalized * (GravitationalPull / distance) * rb.mass * Time.fixedDeltaTime);
+            }
+
+        }
+    }
+}
