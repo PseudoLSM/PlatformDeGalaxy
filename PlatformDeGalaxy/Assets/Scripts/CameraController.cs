6d7faf0978a56fc31516467a729cc805d4d519da
Anders <72574286+PseudoLSM@users.noreply.github.com>
2020-10-28 21:10:57 -0700

    Made a script for the Camera to follow the player

---
+++ PlatformDeGalaxy/Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] protected Transform TargetObject; // Set object to follow (playerCharacter)
+
+    protected Vector3 PositionOffset = new Vector3(0, 0, -1); // A offset for the camera if we ever need it
+
+    public bool LookAtTarget = true; // Change if the camera is following or not (this might be useful for the smooth planet switching
+
+    protected Transform myTransform;
+
+    void Start()
+    {
+        myTransform = GetComponent<Transform>(); // Gets position of the Camera
+    }
+
+    void LateUpdate() // Updates near the end of every frame
+    {
+        if (TargetObject)
+        {
+            myTransform.position = TargetObject.position + PositionOffset; // Actually moves the camera
+
+            if (LookAtTarget) // Dont know why this is here, I don't think we need it.
+            {
+                myTransform.LookAt(TargetObject);
+            }
+        }
+    }
+}
